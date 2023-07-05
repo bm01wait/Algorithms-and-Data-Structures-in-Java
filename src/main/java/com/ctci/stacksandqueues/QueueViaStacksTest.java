da0ca0ffdaaee11b7b609c3f6cdc1b0fffded50d
bm01wait <121624213+bm01wait@users.noreply.github.com>
2023-07-05 14:15:26 +0700

    g

--- src/main/java/com/ctci/stacksandqueues/QueueViaStacksTest.java
+++ src/main/java/com/ctci/stacksandqueues/QueueViaStacksTest.java
@@ -2,10 +2,10 @@ package com.ctci.recursionanddp;
 import org.junit.runner.JUnitCore;
 import org.junit.runner.Result;
 import org.junit.runner.notification.Failure;
-public class FibonacciNumberTest {
+public class QueueViaStacksTest {
     public static void main(String[] args) {
         //testAssertions();
-        Result result = JUnitCore.runClasses(FibonacciNumber.class);
+        Result result = JUnitCore.runClasses(QueueViaStacks.class);
         for (Failure failure : result.getFailures()) {
             System.out.println(failure.toString());
         }
