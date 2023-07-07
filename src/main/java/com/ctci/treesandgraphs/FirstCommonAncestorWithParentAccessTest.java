b3e607a1ab67e09ac18240f0daec0d7dc27471df
bm01wait <121624213+bm01wait@users.noreply.github.com>
2023-07-07 08:31:45 +0700

    jjh

---
+++ src/main/java/com/ctci/treesandgraphs/FirstCommonAncestorWithParentAccessTest.java
@@ -0,0 +1,17 @@
+package com.ctci.treesandgraphs;
+
+import com.ctci.linkedlists.RemoveDuplicates;
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+
+public class FirstCommonAncestorWithParentAccessTest {
+    public static void main(String[] args) {
+        //testAssertions();
+        Result result = JUnitCore.runClasses(RemoveDuplicates.class);
+        for (Failure failure : result.getFailures()) {
+            System.out.println(failure.toString());
+        }
+        System.out.println("Result == "+result.wasSuccessful());
+    }
+}
